e78502b5ccf79b839ffe0e4ad65bb40f14e4c634
TahaAmja <tahap799@gmail.com>
2024-10-30 18:09:26 +0500

    Update pf 2 7.c

--- pf 2 7.c
+++ pf 2 7.c
@@ -57,4 +57,43 @@ int search_string(char arr[ROWS][COLS], const char *str) {
 
 int main() {
     char arr[ROWS][COLS];
-    int score = 0
+    int score = 0;
+    char input[100];
+
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Populate the array with random characters
+    for (int i = 0; i < ROWS - 1; i++) {
+        for (int j = 0; j < COLS; j++) {
+            arr[i][j] = 'A' + rand() % 26; // Random letter A-Z
+        }
+    }
+
+    // Set the last row with student ID's last four digits (example: 1234)
+    arr[ROWS - 1][0] = '1';
+    arr[ROWS - 1][1] = '2';
+    arr[ROWS - 1][2] = '3';
+    arr[ROWS - 1][3] = '4';
+    arr[ROWS - 1][4] = 'Q'; // Optional character
+
+    while (1) {
+        print_array(arr);
+printf("Enter a string to search (or type 'END' to quit): ");
+        scanf("%s", input);
+
+        if (strcmp(input, "END") == 0) {
+            break;
+        }
+
+        if (search_string(arr, input)) {
+            score++;
+            printf("%s is present Score %d\n", input, score);
+        } else {
+            score--;
+            printf("%s is not present Score %d\n", input, score);
+        }
+    }
+
+    return 0;
+}
